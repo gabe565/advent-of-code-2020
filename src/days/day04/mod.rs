41f3b6a67065415206993a56073162ea52d15b86
Gabe Cook <gabe565@gmail.com>
2021-08-02 02:18:40 -0500

    :sparkles: Add day 4 solution

---
+++ src/days/day04/mod.rs
@@ -0,0 +1,53 @@
+use passport::Passport;
+
+mod passport;
+
+const PART1_EXAMPLE_INPUT: &'static str = include_str!("part1_example.txt");
+const PART2_INVALID_INPUT: &'static str = include_str!("part2_invalid_example.txt");
+const PART2_VALID_INPUT: &'static str = include_str!("part2_valid_example.txt");
+const PROBLEM_INPUT: &'static str = include_str!("problem.txt");
+
+fn part1(input: &'static str) -> i32 {
+    let mut count = 0;
+    for i in input.split("\n\n") {
+        let passport = Passport::from_str(i);
+        if passport.part1_is_valid() {
+            count += 1;
+        }
+    }
+    count
+}
+
+fn part2(input: &'static str) -> i32 {
+    let mut count = 0;
+    for i in input.split("\n\n") {
+        let passport = Passport::from_str(i);
+        if passport.part2_is_valid() {
+            count += 1;
+        }
+    }
+    count
+}
+
+pub fn main() {
+    println!("4.1 Example: {}", part1(PART1_EXAMPLE_INPUT));
+    println!("4.1 Problem: {}", part1(PROBLEM_INPUT));
+    println!("4.2 Invalid Example: {}", part2(PART2_INVALID_INPUT));
+    println!("4.2 Valid Example: {}", part2(PART2_VALID_INPUT));
+    println!("4.2 Problem: {}", part2(PROBLEM_INPUT));
+}
+
+#[test]
+fn test_example_1() {
+    assert_eq!(part1(PART1_EXAMPLE_INPUT), 2);
+}
+
+#[test]
+fn test_example_2_invalid() {
+    assert_eq!(part2(PART2_INVALID_INPUT), 0);
+}
+
+#[test]
+fn test_example_2_valid() {
+    assert_eq!(part2(PART2_VALID_INPUT), 4);
+}
---
+++ src/days/day04/passport.rs
@@ -0,0 +1,145 @@
+pub struct Passport {
+    byr: Option<&'static str>,
+    iyr: Option<&'static str>,
+    eyr: Option<&'static str>,
+    hgt: Option<&'static str>,
+    hcl: Option<&'static str>,
+    ecl: Option<&'static str>,
+    pid: Option<&'static str>,
+    cid: Option<&'static str>,
+}
+
+#[allow(dead_code)]
+impl Passport {
+    pub fn new() -> Self {
+        Self {
+            byr: None,
+            iyr: None,
+            eyr: None,
+            hgt: None,
+            hcl: None,
+            ecl: None,
+            pid: None,
+            cid: None,
+        }
+    }
+
+    pub fn from_str(input: &'static str) -> Self {
+        let mut passport = Passport::new();
+        let kv = input.split_whitespace().map(
+            |x| x.split_once(":").unwrap()
+        );
+        for (k, v) in kv {
+            passport.set(k, v);
+        }
+        passport
+    }
+
+    pub fn set(&mut self, k: &'static str, v: &'static str) {
+        match k {
+            "byr" => self.byr = Some(v),
+            "iyr" => self.iyr = Some(v),
+            "eyr" => self.eyr = Some(v),
+            "hgt" => self.hgt = Some(v),
+            "hcl" => self.hcl = Some(v),
+            "ecl" => self.ecl = Some(v),
+            "pid" => self.pid = Some(v),
+            "cid" => self.cid = Some(v),
+            _ => panic!("invalid value {}", k),
+        }
+    }
+
+    pub fn part1_is_valid(&self) -> bool {
+        !self.byr.is_none() && !self.iyr.is_none() && !self.eyr.is_none() &&
+            !self.hgt.is_none() && !self.hcl.is_none() && !self.ecl.is_none() &&
+            !self.pid.is_none()
+    }
+
+    pub fn byr_is_valid(&self) -> bool {
+        match self.byr {
+            Some(i) =>
+                match i.parse::<i32>() {
+                    Ok(i) => 1920 <= i && i <= 2002,
+                    Err(_e) => false,
+                },
+            None => false,
+        }
+    }
+
+    pub fn iyr_is_valid(&self) -> bool {
+        match self.iyr {
+            Some(i) =>
+                match i.parse::<i32>() {
+                    Ok(i) => 2010 <= i && i <= 2020,
+                    Err(_e) => false,
+                },
+            None => false,
+        }
+    }
+
+    pub fn eyr_is_valid(&self) -> bool {
+        match self.eyr {
+            Some(i) =>
+                match i.parse::<i32>() {
+                    Ok(i) => 2020 <= i && i <= 2030,
+                    Err(_e) => false,
+                },
+            None => false,
+        }
+    }
+
+    pub fn hgt_is_valid(&self) -> bool {
+        match self.hgt {
+            Some(i) => {
+                let (str, unit) = i.split_at(i.len() - 2);
+                let hgt = str.parse::<i32>();
+                match hgt {
+                    Ok(i) =>
+                        match unit {
+                            "cm" => 150 <= i && i <= 193,
+                            "in" => 59 <= i && i <= 76,
+                            _ => false,
+                        },
+                    Err(_e) => false,
+                }
+            }
+            None => false,
+        }
+    }
+
+    pub fn hcl_is_valid(&self) -> bool {
+        match self.hcl {
+            Some(i) => i.starts_with('#') && i.len() == 7
+                && i.chars().any(|c| matches!(c, 'a'..='f' | '0'..='9')),
+            None => false,
+        }
+    }
+
+    pub fn ecl_is_valid(&self) -> bool {
+        match self.ecl {
+            Some(i) =>
+                match i {
+                    "amb" | "blu" | "brn" | "gry" | "grn" | "hzl" | "oth" => true,
+                    _ => false,
+                },
+            None => false,
+        }
+    }
+
+    pub fn pid_is_valid(&self) -> bool {
+        match self.pid {
+            Some(i) => i.len() == 9 && i.parse::<i32>().is_ok(),
+            None => false,
+        }
+    }
+
+    pub fn cid_is_valid(&self) -> bool {
+        true
+    }
+
+    pub fn part2_is_valid(&self) -> bool {
+        self.byr_is_valid() && self.iyr_is_valid() && self.eyr_is_valid()
+            && self.hgt_is_valid() && self.hcl_is_valid() && self.ecl_is_valid()
+            && self.pid_is_valid() && self.cid_is_valid()
+    }
+}
